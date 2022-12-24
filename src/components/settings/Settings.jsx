b5b90bc6a4c19f5893eb4175647a58b8aadee282
James <engrjayt200@gmail.com>
2022-12-24 15:21:26 +0100

    Update settings page with card details component

--- src/components/settings/Settings.jsx
+++ src/components/settings/Settings.jsx
@@ -7,96 +7,97 @@ const Settings = () => {
   return (
     <div className="flex flex-col items-start">
     <h1 className="mb-2 font-bold text-lg text-gray-500"> Settings</h1>
-    <p className="text-base leading-relaxed text-left text-gray-400"> Manage your team and preference here </p>
+    <p className="text-base leading-relaxed text-left text-gray-500"> Manage your team and preference here. </p>
+
     <ul className='flex mt-4'>
     {settingsData.map((item) =>(
         <li key={item.id} >
-        <a href='/' className='bg-white border inline-block py-2 px-5 hover:text-blue-darker font-semibold'>{item.title}</a>
+        <a href='/' className=' text-gray-500 border inline-block py-2 px-5 font-semibold'>{item.title}</a>
         </li>
     ))}
 </ul>
 
+<div className='col-span-3 items-start justify-start flex flex-col w-full pt-4 mb-6'>
+<h3 className="py-3 font-semibold text-lg text-gray-600"> Payment Method</h3>
+<p className="text-base leading-relaxed text-left text-gray-500">Update your billing details and address</p>
+</div>
 
-<h3 className="py-6 font-bold text-lg text-gray-500"> Payment Method</h3>
-<p className="text-base leading-relaxed text-left text-gray-400">Update your billing details and address</p>
 
-{/* contact */}
-
-<div className="container px-2 py-4 mx-auto">
-<div class="mt-8 lg:-mx-6 lg:flex lg:items-center">
-  <div class="mt-4 lg:w-1/2 lg:mt-0 lg:mx-6">
-<h4 className="font-bold text-md text-gray-500"> Contact Email</h4>
-<p className="text-base leading-relaxed text-gray-400">Where should invoice be sent ?</p>
+<div class="grid grid-cols-4 border-t">
+  <div className="py-4 col-span-2">
+<div class="mt-4 lg:w-1/2 lg:mt-0">
+<h4 className="font-semibold text-md text-gray-500 flex"> Contact Email</h4>
+<p className="text-base leading-relaxed text-gray-500 flex">Where should invoice be sent ?</p>
+ </div>
  </div>
 
-<div class="md:w-1/2">
-<div className="flex items-center">
-<div className="bg-white border-2 rounded-md w-5 h-5 pr-4 flex flex-shrink-0 justify-center items-center relative">
-<input placeholder="checkbox" type="checkbox" className="focus:opacity-100 checkbox opacity-0 absolute cursor-pointer w-full h-full" />
+ 
+<div className="px-2 py-4">
+<div class="flex pt-4 lg:w-full lg:mt-0">
+<input type="radio" className="h-4 w-4 text-purple-500 mr-3 mt-1" />
+<p class="text-gray-700 text-base font-medium leading-none">Send to my account email</p>
 </div>
-<div>
-<p className="text-base font-medium leading-none text-gray-700 mr-2">Send to my account email</p>
-<span>olivia@untitledui.com</span>
-</div>
-</div>
-
-<div class="flex items-center pt-4">
-<input type="radio" className="h-4 w-4 text-purple-500 mr-3" />
+<p className='text-gray-500'>olivia@untitledui.com</p>
+<div class="flex pt-1">
+<input type="radio" className="h-4 w-4 text-purple-500 mr-3 mt-1" />
 <p class="text-gray-700">Send to an alternative email</p>
 </div>
-<div class="md:w-1/2 bg-white border border-slate-300 w-full h-13 shadow-lg rounded-md gap-4 pt-4 p-4 mt-6 flex flex-row">
-    <h1 class="text-base font-semibold text-zinc-800">billing@untitledui.com</h1>
+<div class="bg-white border border-slate-10 h-2 pb-8 shadow-sm rounded-md pt-1 p-6 mt-6 flex flex-col w-[350px] mx-auto items-start">
+    <h1 class="text-base font-semibold text-gray-600">billing@untitledui.com</h1>
 	</div>
-    </div>
-    </div>
-    </div>
-   
-
-
-  <div className="container px-2 py-4 mx-auto">
-<div class="mt-8 lg:-mx-6 lg:flex lg:items-center">
-  <div class="mt-4 lg:w-1/2 lg:mt-0 lg:mx-6">
-<h4 className="font-bold text-md text-gray-500"> Card Details</h4>
-<p className="text-base leading-relaxed text-gray-400">Select default payment method</p>
+ </div>
  </div>
 
-<div class="md:w-1/2 px-3">
-	<div class="w-full bg-purple-50 rounded-lg shadow-sm p-5 border border-purple-500 flex flex-col sm:flex-row justify-between items-center gap-2 sm:gap-0">
+
+ {/* Visa Card details */}
+ <div class="grid grid-cols-4 border-t">
+  <div className="py-4 col-span-2">
+<div class="mt-4 lg:mt-0">
+<h4 className="font-semibold text-md text-gray-500 flex"> Card details</h4>
+<p className="text-base leading-relaxed text-gray-500 flex">Select default payment method</p>
+ </div>
+ </div>
+
+	<div class="w-[510px]  bg-purple-50 rounded-lg shadow-sm p-1 border border-purple-500 flex flex-col sm:flex-row justify-between items-center mt-6 gap-2 sm:gap-0">
 		<div class="flex flex-col sm:flex-row justify-start items-center gap-4">
 			<div class="flex rounded-md bg-white py-2 px-2">
                 <img src={VisaLogo} alt='visa logo' />
             </div>
 			<div className="text-center sm:text-left">
-				<p className="text-purple-500 font-semibold">Visa ending in 1234</p>
-				<p className="text-purple-500 font-semibold">Expiry 06/2024</p>
-                <p className="text-purple-500 font-semibold pt-4">Set as default <span className='text-purple-500 px-2'>Edit</span></p>
+				<p className="text-purple-700 text-sm font-semibold">Visa ending in 1234</p>
+				<p className="text-purple-500 text-sm">Expiry 06/2024</p>
+                <p className="text-purple-500 text-sm pt-1">Set as default <span className='text-purple-700 px-2 font-semibold'>Edit</span></p>
 			</div>
 		</div>
-        <img src={Checkicon} alt='check icon' className="bg-blue-700 py-1 px-1 text-white rounded-lg hover:bg-blue-600" />
+        <img src={Checkicon} alt='check icon' className="bg-purple-600 h-4 w-4 py-1 px-1 rounded-lg mb-9 mr-2" />
 	</div>
+</div>
 
+{/* Mater Card details */}
 
-    <div class="md:w-full pt-6">
-	<div class="w-full bg-purple-50 rounded-lg shadow-sm p-5 border border-purple-500 flex flex-col sm:flex-row justify-between items-center gap-2 sm:gap-0">
+<div class="grid grid-cols-4 border-t">
+  <div className="py-4 col-span-2">
+<div class="mt-4 lg:mt-0">
+<h4 className="font-semibold text-md text-gray-500 flex"> Card details</h4>
+<p className="text-base leading-relaxed text-gray-500 flex">Select default payment method</p>
+ </div>
+ </div>
+
+	<div class="w-[510px]  bg-white rounded-lg shadow-sm p-1 border flex flex-col sm:flex-row justify-between items-center mt-6 gap-2 sm:gap-0">
 		<div class="flex flex-col sm:flex-row justify-start items-center gap-4">
 			<div class="flex rounded-md bg-white py-2 px-2">
                 <img src={Mastercardicon} alt='visa logo' />
             </div>
 			<div className="text-center sm:text-left">
-				<p className="text-purple-500 font-semibold">Visa ending in 1234</p>
-				<p className="text-purple-500 font-semibold">Expiry 06/2024</p>
-                <p className="text-purple-500 font-semibold pt-4">Set as default <span className='text-purple-500 px-2'>Edit</span></p>
+				<p className="text-gray-600 text-sm font-semibold">Mastercard ending in 1234</p>
+				<p className="text-gray-500 text-sm">Expiry 06/2024</p>
+                <p className="text-gray-600 text-sm pt-1">Set as default <span className='text-purple-700 px-2 font-semibold'>Edit</span></p>
 			</div>
 		</div>
-        <img src={Checkicon} alt='check icon' className="bg-blue-700 py-1 px-1  text-white rounded-lg hover:bg-blue-600" />
+        <img src={Checkicon} alt='check icon' className="bg-white h-4 w-4 py-1 px-1 border-2 rounded-lg mb-9 mr-2" />
 	</div>
-    </div>
-
-
-
-    </div>
-    </div>
-  </div>
+</div>
+<p className='w-full ml-12 mt-2 text-gray-500 font-semibold'> + Add payment method</p> 
 </div>
   )
 }
